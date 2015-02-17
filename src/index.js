496b8d39a3fdc38024b807c37fc21b3e3f52fefe
Mauricio Poppe <me@maurizzzio.com>
2015-02-16 20:31:38 -0400

    documented index.js

--- src/index.js
+++ src/index.js
@@ -13,8 +13,22 @@ var renderer, oldRenderer;
 var pojoviz;
 
 /**
+ * Given an inspector instance it build the graph and also the
+ * layout of the nodes belonging to it, the resulting object is
+ * an object which is used by a renderer to be drawn
  *
- * @return {Object} [description]
+ * @return {Object} return An object with the following info:
+ *  {
+ *     nodes: [array of objects, each having label,x,y,height,
+ *            width,properties,successors,predecessors],
+ *     edges: [array of objects, each having to,from,property],
+ *     center: an object with the center of the bbox that covers
+ *            the layout of the graph
+ *     mn: an object with info about the minimum x,y of the bbox
+ *            that covers the layout of the graph
+ *     mx: an object with info about the maximum x,y of the bbox
+ *            that covers the layout of the graph
+ *  }
  */
 function process(inspector) {
   var g = new dagre.Digraph(),
@@ -24,7 +38,6 @@ function process(inspector) {
       libraryNodes = str.nodes,
       libraryEdges = str.edges;
 
-  //console.log(str);
   // create the graph
   // each element of the graph has
   // - label
@@ -56,7 +69,7 @@ function process(inspector) {
     });
   });
 
-  // layout of the graph
+  // generate the graph layout
   var layout = dagre.layout()
     .nodeSep(30)
     // .rankSep(70)
@@ -71,10 +84,10 @@ function process(inspector) {
       total = g.nodes().length;
 
   // update the node info adding:
-  // - x
-  // - y
-  // - predecessors
-  // - successors
+  // - x (x-coordinate of the center of the node)
+  // - y (y-coordinate of the center of the node)
+  // - predecessors (an array with the identifiers of the predecessors of this node)
+  // - successors (an array with the identifiers of the successor of this node)
   layout.eachNode(function (k, layoutInfo) {
     var x = layoutInfo.x;
     var y = layoutInfo.y;
@@ -122,7 +135,15 @@ function process(inspector) {
   };
 }
 
-// render
+/**
+ * Draws the current inspector in the canvas with the following steps:
+ *
+ * - clears the canvas
+ * - processes the data of the current inspector
+ * - renders the data produced by the method above
+ * - notifies the user of any action performed
+ *
+ */
 function render() {
   var data;
 
@@ -159,20 +180,44 @@ function render() {
 
 // public api
 pojoviz = {
+  /**
+   * holds a list of all the renderers available, shipped
+   * with a d3 and a ThreeJS renderer
+   */
   renderers: {},
+  /**
+   * Adds a renderer to the renderers available
+   * @param newRenderers
+   */
   addRenderers: function (newRenderers) {
     _.merge(pojoviz.renderers, newRenderers);
   },
+  /**
+   * Clears the inspector variable
+   * @chainable
+   */
   unsetInspector: function () {
     oldInspector = inspector;
     inspector = null;
+    return this;
   },
+  /**
+   * Gets the current inspector (set through #setCurrentInspector)
+   * @returns {*}
+   */
   getCurrentInspector: function () {
     return inspector;
   },
   /**
+   * Given an object containing the configuration options of a
+   * possible new instance of Inspector, this method checks if there's
+   * already an instance with the same displayName/entryPoint to avoid
+   * creating more Instances of the same type, calls the hook
+   * `modifyInstance` after the inspector is retrieved/created
    *
-   * @param options
+   * @param {config} options Options passed to an Inspector instance
+   * if the entryPoint/displayName wasn't created yet in
+   * InspectorInstances
    * @returns {Promise}
    */
   setCurrentInspector: function (options) {
@@ -190,10 +235,18 @@ pojoviz = {
     inspector.modifyInstance(options);
     return inspector.init();
   },
+  /**
+   * Updates the value of the current renderer
+   * @param r
+   */
   setRenderer: function (r) {
     oldRenderer = renderer;
     renderer = pojoviz.renderers[r];
   },
+  /**
+   * Gets the current renderer
+   * @returns {*}
+   */
   getRenderer: function () {
     return renderer;
   },
@@ -205,7 +258,9 @@ pojoviz = {
   analyzer: {
     Inspector: require('./analyzer/Inspector')
   },
-  utils: require('./util')
+  utils: require('./util'),
+  // used in search to save the downloaded configurations
+  userVariables: []
 };
 
 // custom events

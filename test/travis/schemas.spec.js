68bd3cef467b7882cb5710fe307949991f5a0db6
Mauricio Poppe <me@maurizzzio.com>
2015-02-18 01:24:18 -0400

    e2e test modified to include only some sample configurations

--- test/travis/schemas.spec.js
+++ test/travis/schemas.spec.js
@@ -12,26 +12,33 @@ describe('Known configuration schemas', function () {
     this.timeout(3 * 60 * 1000);
 
     var promises = [];
-    var schemas = pojoviz.schemas;
-    utils.notification = function () {};
-    for (var k in schemas) {
-      if (schemas.hasOwnProperty(k)) {
-        schemas[k].forEach(function (cfg) {
-          cfg.debug = true;
-          if (cfg.src) {
-            promises.push(
-              pojoviz.setCurrentInspector(cfg)
-                .then(function () {
-                  console.time('process');
-                  console.log('processing: ', cfg.displayName || cfg.entryPoint);
-                  pojoviz.process(pojoviz.getCurrentInspector());
-                  console.timeEnd('process');
-                })
-            );
-          }
-        });
+
+    var schemas = [{
+      label: 'EmberJS',
+      src: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js|//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.1.2/handlebars.js|//cdnjs.cloudflare.com/ajax/libs/ember.js/1.6.1/ember.js',
+      entryPoint: 'Ember',
+      forbiddenTokens: 'global:$|global:Handlebars|pojoviz:builtIn|global:window|global:document'
+    }, {
+      entryPoint: 'Phaser',
+      src: '//cdnjs.cloudflare.com/ajax/libs/phaser/2.0.6/phaser.min.js',
+      analyzerConfig: {
+        visitSimpleFunctions: true
       }
-    }
+    }];
+
+    utils.notification = function () {};
+    schemas.forEach(function (cfg) {
+      cfg.debug = true;
+      promises.push(
+        pojoviz.setCurrentInspector(cfg)
+          .then(function () {
+            console.time('process');
+            console.log('processing: ', cfg.displayName || cfg.entryPoint);
+            pojoviz.process(pojoviz.getCurrentInspector());
+            console.timeEnd('process');
+          })
+      );
+    });
 
     Q.all(promises)
       .then(function () {
